0c739d657a0a53ce1769b36c9b8b43c421379845
Destroyt1 <108230468+Destroyt1@users.noreply.github.com>
2022-09-08 00:57:17 +0400

    add js/

--- js/scroll2.js
+++ js/scroll2.js
@@ -1,5 +1,5 @@
   const swipe = new Swiper('.swiper', {
-    allowTouchMove: false,
+    // allowTouchMove: false,
     slidesPerView: 3,
     slidesPerGroup: 1,
     loop: true,
@@ -22,7 +22,7 @@
 if (mediaQuery.matches) {
   // я выполнюсь только если ширина экрана 795 или больше
   const swipe = new Swiper('.swiper', {
-    allowTouchMove: false,
+    // allowTouchMove: false,
     slidesPerView: 2,
     slidesPerGroup: 1,
     loop: true,
@@ -39,3 +39,36 @@ if (mediaQuery.matches) {
 
   });
 }
+
+var mediaQuery = window.matchMedia('(max-width: 390px)');
+
+if (mediaQuery.matches) {
+  // я выполнюсь только если ширина экрана 795 или больше
+  const swipe = new Swiper('.swiper', {
+    // allowTouchMove: false,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    loop: true,
+    // spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+
+  });
+}
+
+
+const btnMore = document.querySelector('.awards-more');
+const awardsItems = document.querySelectorAll('.awards__list-item');
+
+btnMore.addEventListener('click', () => {
+  awardsItems.forEach(el => {el.classList.add('awards__list-item--visable') });
+  btnMore.closest('.awards-center').classList.add('awards-center--hidden');
+});
+
